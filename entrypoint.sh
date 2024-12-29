5e7394708f4a8a36669fb8e8f23c23af29f537ca
Aesgarth <getflair@gmail.com>
2024-12-29 10:50:47 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ service cups start
 # Wait for CUPS to initialize
 sleep 2
 
+# Configure USB printer if connected
+if [ -e /dev/usb/lp0 ]; then
+  lpadmin -p USB_Printer -E -v usb:/dev/usb/lp0 -m everywhere
+fi
+
 # Configure the printer (Replace with your printer details)
 lpadmin -p My_Printer -E -v ipp://printer_ip/printer_queue -m everywhere
 cupsctl --remote-admin --remote-any --share-printers
