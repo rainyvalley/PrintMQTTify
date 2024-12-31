e4a387338584a90907d9a4ba877bc363ca55273e
Aesgarth <getflair@gmail.com>
2024-12-31 13:15:30 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ cp /app/cupsd.conf /etc/cups/cupsd.conf
 
 # Ensure permissions are correct
 ## The following 2 lines are specific to the Sewoo printer I have been using. 
-chmod 755 /usr/lib/cups/filter/rastertosewoo
-chmod +x /usr/lib/cups/filter/rastertosewoo
+#chmod 755 /usr/lib/cups/filter/rastertosewoo
+#chmod +x /usr/lib/cups/filter/rastertosewoo
 # The following 2 lines are for the cups config file
 chmod 644 /etc/cups/cupsd.conf
 chown root:lp /etc/cups/cupsd.conf
@@ -21,14 +21,14 @@ sleep 2
 
 # Configure USB printer if connected
 # THis should be modified or removed to match your printer
-if [ -e /dev/usb/lp0 ]; then
-  lpadmin -p My_Printer -E -v usb:/dev/usb/lp0 -m everywhere
-fi
+#if [ -e /dev/usb/lp0 ]; then
+#  lpadmin -p My_Printer -E -v usb:/dev/usb/lp0 -m everywhere
+#fi
 
 #Install drivers - Modify for your own printer and driver
 #This driver is for SEWOO LKT series printers. Tested only on an LK-T100 printer.
 #Modify for your own needs.
-lpadmin -p My_Printer -E -v usb:/dev/usb/lp0 -m SEWOOLKT.ppd 
+#lpadmin -p My_Printer -E -v usb:/dev/usb/lp0 -m SEWOOLKT.ppd 
 
 # Configure the printer (Replace with your printer details)
 # lpadmin -p My_Printer -E -v ipp://printer_ip/printer_queue -m everywhere
